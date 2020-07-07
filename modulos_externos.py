6ba8d3959165fe7924dae5923d73774425ded689
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-07-07 18:08:06 -0300

    Módulos Externos

---
+++ modulos_externos.py
@@ -0,0 +1,22 @@
+"""
+Módulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+
+Você pode conhecer todos os pacotes externos oficiais em: https://pypi.org
+
+Pacote colorama -> É utilizado para permitir impressão de textos coloridos no terminal.
+
+instalando um módulo:
+
+pip install nome_do_modulo
+
+
+Importando um Módulo
+
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'some red text')
+print(Fore.GREEN + 'and with a green background')
+
+"""
